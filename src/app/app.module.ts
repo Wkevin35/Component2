00bb6706a01458130e7a5213ca13827de8a595a0
kevin <wangk0335@gmail.com>
2022-02-22 21:53:16 +0800

    feat():test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ToolbarModule } from './toolbar/toolbar.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
